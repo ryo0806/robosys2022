2869c3a0368ed3e5a3322145ca079d30f4b2af7c
ryo yanagisawa <s21c1132yt@s.chibakoudai.jp>
2022-10-27 11:42:24 +0900

    Add a command

---
+++ test.bash
@@ -0,0 +1,34 @@
+#!/bin/bash -xv
+# SPDX-FlieCopyrightText: 2022 yanagisawa ryo
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng (){
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+out=$(echo あ | ./plus)
+[ "$?" =1 ]       || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" =1 ]       || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
+
