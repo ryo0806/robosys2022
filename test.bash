68f1d5c5343560959c143edb3d6120181909351d
ryo yanagisawa <s21c1132yt@s.chibakoudai.jp>
2022-11-30 03:17:21 +0900

    re:test.bash

--- test.bash
+++ test.bash
@@ -2,24 +2,24 @@
 # SPDX-FlieCopyrightText: 2022 yanagisawa ryo
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng (){
-	echo NG at Line $1
-	res=1
+ng ( ) {
+    echo NG at Line $1
+    res=1
 }
 
 res=0
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+  out=$(seq 5 | ./plus)
+  [ "${out}" = 15 ] || ng ${LINENO}
 
-out=$(echo あ | ./plus)
-[ "$?" =1 ]       || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+  out=$(echo あ | ./plus)
+  [ "$?" =1 ]      || ng ${LINENO}
+  [ "${out}" = "" ]|| ng ${LINENO}
 
-out=$(echo | ./plus)
-[ "$?" =1 ]       || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+  out=$(echo | ./plus)
+  [ "$?" =1 ]      || ng ${LINENO}
+  [ "${out}" = "" ]|| ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 
-exit $res
+  exit $res
